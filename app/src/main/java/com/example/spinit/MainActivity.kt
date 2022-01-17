ef866463c0efeef2f8510e0b0f6e35c07b385e2d
mixaneko <86652770+mixaneko@users.noreply.github.com>
2022-01-17 21:56:33 +0300

    fixnew(summator)

--- app/src/main/java/com/example/spinit/MainActivity.kt
+++ app/src/main/java/com/example/spinit/MainActivity.kt
@@ -2,11 +2,44 @@ package com.example.spinit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import com.example.spinit.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  sum:TextView
+    lateinit var zadanieclass: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        zadanieclass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(zadanieclass.root)
+        val editText1:EditText=findViewById(R.id.chislo1)
+        val editText2:EditText=findViewById(R.id.chislo2)
+        sum=findViewById(R.id.summa)
+
+        zadanieclass.button.setOnClickListener{
+            val a=editText1.text.toString().toIntOrNull()?:0
+            val b= editText2.text.toString().toIntOrNull()?:0
+            val summa=a+b
+             sum.text=summa.toString()
+            }
+
+
+        }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("Sum", sum.toString())
 
     }
-}
+
+
+
+
+
+
+    }
+
+
