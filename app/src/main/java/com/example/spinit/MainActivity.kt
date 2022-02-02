31a33fcba99fd6d394de8af7c7690568c6e97b87
Sergey <serega.putkin@yandex.ru>
2022-02-02 12:07:14 +0300

    fix

--- app/src/main/java/com/example/spinit/MainActivity.kt
+++ app/src/main/java/com/example/spinit/MainActivity.kt
@@ -2,44 +2,45 @@ package com.example.spinit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import com.example.spinit.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var  sum:TextView
-    lateinit var zadanieclass: ActivityMainBinding
+    lateinit var sum: TextView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        zadanieclass = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(zadanieclass.root)
-        val editText1:EditText=findViewById(R.id.chislo1)
-        val editText2:EditText=findViewById(R.id.chislo2)
-        sum=findViewById(R.id.summa)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        val editText1: EditText = findViewById(R.id.chislo1)
+        val editText2: EditText = findViewById(R.id.chislo2)
+        sum = findViewById(R.id.summa)
 
-        zadanieclass.button.setOnClickListener{
-            val a=editText1.text.toString().toIntOrNull()?:0
-            val b= editText2.text.toString().toIntOrNull()?:0
-            val summa=a+b
-             sum.text=summa.toString()
-            }
-
-
+        binding.button.setOnClickListener {
+            val a = editText1.text.toString().toIntOrNull() ?: 0
+            val b = editText2.text.toString().toIntOrNull() ?: 0
+            val summa = a + b
+            sum.text = summa.toString()
         }
+
+
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString("Sum", sum.toString())
+        outState.putString("Sum", sum.text.toString())
 
     }
 
-
-
-
-
-
+// На тебе работающий код. Жду обьяснительную почему в твоей версии это не работало
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val sum = savedInstanceState.getString("Sum")
+        binding.summa.text = sum
     }
+}
 
 
